3b40ed33e1f64fab884c68107d5de1d54dbc2a71
georgi <gvmitrev@gmail.com>
2013-06-06 17:14:39 +0300

    Better formatting

--- lib/wololo_formatter/active_support.rb
+++ lib/wololo_formatter/active_support.rb
@@ -18,13 +18,13 @@ module ActiveSupport
       when "DEBUG"
         "\033[1;34;40m[DEBUG]\033[0m" # blue
       when "INFO"
-        "\033[0;32;40m[INFO]\033[0m" # bold white
+        "\033[0;32;40m[INFO]\033[0m " # bold white
       when "WARN"
-        "\033[0;33;40m[WARNING]\033[0m" # bold yellow
+        "\033[0;33;40m[WARN]\033[0m " # bold yellow
       when "ERROR"
         "\033[1;31;40m[ERROR]\033[0m" # bold red
       when "FATAL"
-      "\033[7;31;40m[FATAL]\033[0m" # bold black, red bg
+        "\033[7;31;40m[FATAL]\033[0m" # bold black, red bg
       else
         "[#{severity}]" # none
       end
--- lib/wololo_formatter/logger.rb
+++ lib/wololo_formatter/logger.rb
@@ -12,9 +12,9 @@ class Logger
       when "DEBUG"
         "\033[1;34;40m[DEBUG]\033[0m" # blue
       when "INFO"
-        "\033[0;32;40m[INFO]\033[0m" # bold white
+        "\033[0;32;40m[INFO]\033[0m " # bold white
       when "WARN"
-        "\033[0;33;40m[WARNING]\033[0m" # bold yellow
+        "\033[0;33;40m[WARN]\033[0m " # bold yellow
       when "ERROR"
         "\033[1;31;40m[ERROR]\033[0m" # bold red
       when "FATAL"
@@ -38,9 +38,9 @@ class Logger
       when "DEBUG"
         "\033[1;34;40m[DEBUG]\033[0m" # blue
       when "INFO"
-        "\033[0;32;40m[INFO]\033[0m" # bold white
+        "\033[0;32;40m[INFO]\033[0m " # bold white
       when "WARN"
-        "\033[0;33;40m[WARNING]\033[0m" # bold yellow
+        "\033[0;33;40m[WARN]\033[0m " # bold yellow
       when "ERROR"
         "\033[1;31;40m[ERROR]\033[0m" # bold red
       when "FATAL"
